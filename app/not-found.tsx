1e4395acc5f70de99b5742c3556f67915b790c44
thierry <thierry.alexandr@gmail.com>
2026-01-05 13:34:29 +0800

    Initial Next.js site

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Page not found</h1>
+      <p>The page you’re looking for doesn’t exist.</p>
+      <a href="./">Go home</a>
+    </main>
+  );
+}
